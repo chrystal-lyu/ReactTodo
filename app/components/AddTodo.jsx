075092790dea8e721bec1e13e75a262dfc31e5c6
Crystal Lu <syyssie@gmail.com>
2017-06-10 19:00:45 +0800

    Created AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var todoText = this.refs.todoText.value;
+
+    if(todoText.length > 0) {
+      this.refs.todoText.value = '';
+      this.props.onAddTodo(todoText);
+    } else {
+      this.refs.todoText.focus();
+    }
+  },
+  render: function() {
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div><input type ="text" ref="todoText" placeholder="What do you need to do?"/></div>
+          <div><button className="button expanded">Add</button></div>
+        </form>
+      </div>
+    )
+  }
+});
+
+module.exports = AddTodo;
