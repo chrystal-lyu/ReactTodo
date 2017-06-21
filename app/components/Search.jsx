f7b59b34aa23f4fef2706d6fc2b46f706e6c0327
Crystal Lu <syyssie@gmail.com>
2017-06-21 16:59:56 +0800

    refactor Search to work with redux store

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,24 +1,35 @@
 var React = require('react');
+var {connect} = require('react-redux');
+var actions = require('actions');
 
-var Search = React.createClass({
-  handleSearch: function() {
-    var showCompleted = this.refs.showCompleted.checked;
-    var searchText = this.refs.searchText.value;
+export var Search = React.createClass({
 
-    this.props.onSearch(showCompleted, searchText);
-  },
   render: function() {
+    var {dispatch, showCompleted, searchText} = this.props;
+
     return(
       <div className="container__header">
         <div>
-          <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
+          <input type="search" ref="searchText" placeholder="Search todos" value={searchText} onChange={() => {
+              var searchText = this.refs.searchText.value;
+                dispatch(actions.setSearchText(searchText));
+            }}/>
       </div>
         <div>
-          <label><input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>Show completed todos</label>
+          <label><input type="checkbox" ref="showCompleted" checked={showCompleted} onChange={() => {
+              dispatch(actions.toggleShowCompleted());
+            }}/>Show completed todos</label>
         </div>
       </div>
     );
   }
 });
 
-module.exports = Search;
+export default connect(
+  (state) => {
+    return {
+      showCompleted: state.showCompleted,
+      searchText: state.searchText
+    }
+  }
+)(Search);
