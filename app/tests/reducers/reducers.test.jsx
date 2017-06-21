3cd2bfdb41752bdc16c235874e170f7e3c4389e6
Crystal Lu <syyssie@gmail.com>
2017-06-21 14:38:22 +0800

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -25,4 +25,40 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo completed status', () => {
+      // define todos array with realistic todo item
+      var todos = [
+        {
+          id: 1,
+          text: 'Walk the dog',
+          completed: true,
+          createdAt: 123,
+          completedAt: 125
+        }
+      ];
+      // generate action
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      }
+      // call reducer and assert completed flipped
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
 });
